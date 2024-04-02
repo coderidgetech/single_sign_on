7aec918324d0c8e93765171bc25b31d99c172b1b
arunkumarvegyarapu <arunkumar.vegyarapu@tectoro.com>
2024-04-02 11:52:36 +0530

    "Initial commit"

---
+++ sso_signin/lib/GoogleAuthentication.dart
@@ -0,0 +1,19 @@
+// google_authentication.dart
+
+import 'package:sso_signin/Authenticator.dart';
+
+
+class GoogleAuthentication implements Authenticator {
+  @override
+  Future<void> signInWithEmailPassword(String email, String password) {
+    // TODO: implement signInWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signInWithGoogle() {
+    // TODO: implement signInWithGoogle
+    throw UnimplementedError();
+  }
+ 
+}
