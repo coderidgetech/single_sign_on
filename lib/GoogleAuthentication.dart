9354c2b94a02730744542c783574fe00c13e4389
arunvegyas@gmail.com <arunkumar.vegyarapu@tectoro.com>
2024-05-16 17:16:12 +0530

    "Version 7"

--- lib/GoogleAuthentication.dart
+++ lib/GoogleAuthentication.dart
@@ -1,7 +1,8 @@
 // google_authentication.dart
 
-import 'package:sso_signin/Authenticator.dart';
 
+
+import 'Authenticator.dart';
 
 class GoogleAuthentication implements Authenticator {
   @override
