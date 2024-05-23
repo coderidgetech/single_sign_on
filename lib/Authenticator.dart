db5322495b22f267b4faa337a66c75378fe412d6
Prashanth Algottu <prashanth.algottu@tectoro.com>
2024-05-23 12:14:35 +0530

    intial commit

--- lib/Authenticator.dart
+++ lib/Authenticator.dart
@@ -2,4 +2,5 @@ abstract class Authenticator {
   Future<void> signInWithGoogle();
   Future<void> signInWithEmailPassword(String email, String password);
 // Define other authentication methods
+
 }
