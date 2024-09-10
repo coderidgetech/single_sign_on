ca41205ee87707db4f4a132835762874f4d09388
Prashanth <algottuprashanth@gmail.com>
2024-09-10 12:43:17 +0530

    new code

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_windows_3.19.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\EMM\LOGINs\LOG_IN_MODULE\LOGIN\MODULE\single_sign_on"
+export "FLUTTER_ROOT=/home/prashanth/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/prashanth/EMM/OldData/EMM/LOGINs/LOG_IN_MODULE/LOGIN/MODULE/single_sign_on"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.2"
 export "FLUTTER_BUILD_NUMBER=0.0.2"
